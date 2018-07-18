57337f6d682ccca85a245cb29c59483463b5a3b6
Anthony Clemow <anthony1487@gmail.com>
2018-07-18 18:55:18 -0400

    Initial commit, with react landing page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Nav from './Components/NavBar/NavBar.js'
+import LandingPage from './Components/Landing/Landing';
+
+class App extends Component {
+  render() {
+    return (
+      
+      <div className="App">
+        <Nav></Nav>
+        <LandingPage></LandingPage>
+      </div>
+    );
+  }
+}
+
+export default App;
